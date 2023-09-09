e09aee23dbac6b4e99ca9ed3cc709ca98adc697b
kenneth <1269769296@qq.com>
2023-09-09 16:47:33 +0800

    plot code for cooperative interception

--- MCL_description.m
+++ MCL_description.m
@@ -1,35 +1,67 @@
 clc;clear;
 figure_configuration_IEEE_standard;
 
-%% 数据
-relative_pos_all = zeros(5, 5, 3);  % （时间，粒子id，坐标）
-mav_orient_all = zeros(5, 5, 1);    % 偏航角
-sphere_pos_all = zeros(5, 5, 3);
-measurement_pos_all = zeros(5, 1, 3);
-measurement_pos_sphere = zeros(5, 1, 3);
+%% 数据初始化
+mav_pos_all = zeros(5, 2, 4, 3);    % （时间，飞机id，粒子id，坐标）
+target_pos_all = zeros(5, 2, 4, 3);	% （时间，目标id，粒子id，坐标）
+mav_orient_all = zeros(5, 2, 4, 1);	% 偏航角
+sphere_pos_all = zeros(5, 2, 4, 3);
+measurement_pos_all = zeros(5, 2, 1, 3);
+measurement_pos_sphere = zeros(5, 2, 1, 3);
 
-times = size(relative_pos_all, 1);
-particle_num = size(relative_pos_all, 2);
+times = size(target_pos_all, 1);
+mav_num = size(mav_pos_all,2);
+target_num = size(target_pos_all, 2);
+particle_num = size(target_pos_all, 3);
 
-relative_pos_all(1,:,:) = [0.3, 5, 3; 2.5, 6, 3.3; 1.2, 7.5, 2.8; -0.4, 9, 3.5; 1.8, 10.5, 2.6];
-mav_orient_all(1,:,:) = deg2rad([86; 88; 90; 92; 94]);
-relative_pos_all(2,:,:) = [-0.3, 4.5, 3.2; 2.9, 5.8, 3.5; 1.3, 6.8, 2.7; -1.4, 8.5, 3.4; 2.2, 9.1, 2.7];
-mav_orient_all(2,:,:) = deg2rad([82; 86; 90; 94; 98]);
-relative_pos_all(3,:,:) = [-0.3, 4.5, 3.2; 2.9, 5.8, 3.5; 1.3, 6.8, 2.7; -1.4, 8.5, 3.4; 2.2, 9.1, 2.7];
-mav_orient_all(3,:,:) = deg2rad([82; 86; 90; 94; 98]);
-relative_pos_all(4,:,:) = [1.0, 6.4, 3.0; 1.8, 6.6, 2.5; 1.3, 6.8, 2.7; 2.1, 9.0, 2.6; 2.3, 9.2, 2.8];
-mav_orient_all(4,:,:) = deg2rad([86; 88; 90; 98; 100]);
-relative_pos_all(5,:,:) = [0.6, 5.8, 3.2; 1.6, 6.2, 2.3; 1.2, 6.4, 2.8; 1.7, 8.4, 2.4; 2.1, 8.8, 2.7];
-mav_orient_all(5,:,:) = deg2rad([86; 88; 92; 96; 100]);
-measurement_pos_all(3,1,:) = [1.2, 8, 2.8];
+%% 数据赋值
+mav_pos_all(1,1,:,:) = [-2, 0, 0; -2.2, 0.1, 0; -2.1, 0.2, 0; -1.8, -0.1, 0];
+mav_pos_all(1,2,:,:) = [3, 0, 0; 3.2, 0.2, 0; 3.1, 0.3, 0; 2.8, -0.2, 0];
+target_pos_all(1,1,:,:) = [-3.3, 5, 0; -2.5, 5.6, 0; -1.2, 6.5, 0; -2.4, 6.9, 0];
+target_pos_all(1,2,:,:) = [3.3, 5, 0; 3.5, 5.6, 0; 2.2, 4.5, 0; 2.4, 4.9, 0];
+mav_orient_all(1,1,:,:) = deg2rad([86; 88; 90; 92]);
+mav_orient_all(1,2,:,:) = deg2rad([86; 88; 90; 92]);
+
+mav_pos_all(2,1,:,:) = [-2, 0, 0; -2.2, 0.1, 0; -2.1, 0.2, 0; -1.8, -0.1, 0];
+mav_pos_all(2,2,:,:) = [3, 0, 0; 3.2, 0.2, 0; 3.1, 0.3, 0; 2.8, -0.2, 0];
+target_pos_all(2,1,:,:) = [-3.3, 4.5, 0; -2.9, 5.8, 0; -1.3, 6.8, 0; -2.4, 8.5, 0];
+target_pos_all(2,2,:,:) = [3.3, 4.5, 0; 2.9, 5.8, 0; 2.3, 6.8, 0; 2.4, 8.5, 0];
+mav_orient_all(2,1,:,:) = deg2rad([82; 86; 90; 94]);
+mav_orient_all(2,2,:,:) = deg2rad([82; 86; 90; 94]);
+
+mav_pos_all(3,1,:,:) = [-2, 0, 0; -2.2, 0.1, 0; -2.1, 0.2, 0; -1.8, -0.1, 0];
+mav_pos_all(3,2,:,:) = [3, 0, 0; 3.2, 0.2, 0; 3.1, 0.3, 0; 2.8, -0.2, 0];
+target_pos_all(3,1,:,:) = [-3.3, 4.5, 0; -2.9, 5.8, 0; -1.3, 6.8, 0; -1.4, 8.5, 0];
+target_pos_all(3,2,:,:) = [3.3, 4.5, 0; 2.9, 5.8, 0; 2.3, 6.8, 0; 2.4, 8.5, 0];
+mav_orient_all(3,1,:,:) = deg2rad([82; 86; 90; 94]);
+mav_orient_all(3,2,:,:) = deg2rad([82; 86; 90; 94]);
+measurement_pos_all(3,1,1,:) = [-3.7, 7, 0];  % 观测
+measurement_pos_all(3,2,1,:) = [1.2, 8, 0];
+
+mav_pos_all(4,1,:,:) = [-2, 0, 0; -2.2, 0.1, 0; -2.1, 0.2, 0; -1.8, -0.1, 0];
+mav_pos_all(4,2,:,:) = [3, 0, 0; 3.2, 0.2, 0; 3.1, 0.3, 0; 2.8, -0.2, 0];
+target_pos_all(4,1,:,:) = [-2.0, 6.4, 0; -2.8, 6.6, 0; -2.3, 6.8, 0; -2.1, 9.0, 0];
+target_pos_all(4,2,:,:) = [2.0, 6.4, 0; 2.8, 6.6, 0; 2.3, 6.8, 0; 2.1, 9.0, 0];
+mav_orient_all(4,1,:,:) = deg2rad([86; 88; 90; 98]);
+mav_orient_all(4,2,:,:) = deg2rad([86; 88; 90; 98]);
+
+mav_pos_all(5,1,:,:) = [-2, 0, 0; -2.2, 0.1, 0; -2.1, 0.2, 0; -1.8, -0.1, 0];
+mav_pos_all(5,2,:,:) = [3, 0, 0; 3.2, 0.2, 0; 3.1, 0.3, 0; 2.8, -0.2, 0];
+target_pos_all(5,1,:,:) = [-2.6, 5.8, 0; -1.6, 6.2, 0; -2.2, 6.4, 0; -2.7, 8.4, 0];
+target_pos_all(5,2,:,:) = [2.6, 5.8, 0; 1.6, 6.2, 0; 2.2, 6.4, 0; 2.7, 8.4, 0];
+mav_orient_all(5,1,:,:) = deg2rad([86; 88; 92; 96]);
+mav_orient_all(5,2,:,:) = deg2rad([86; 88; 92; 96]);
 
 for t = 1:times
-    for m = 1:particle_num
-        p = squeeze(relative_pos_all(t,m,:));
-        sphere_pos_all(t,m,:) = p / norm(p);
-        q = squeeze(measurement_pos_all(t,:,:));
-        if norm(q) > 0.1
-            measurement_pos_sphere(t,1,:) = q / norm(q);
+    for j = 1:target_num
+        for l = 1:particle_num
+            p = squeeze(mav_pos_all(t,1,l,:));
+            pt = squeeze(target_pos_all(t,j,l,:));
+            sphere_pos_all(t,j,l,:) = (pt-p) / norm(pt-p);
+            q = squeeze(measurement_pos_all(t,j,1,:));
+            if norm(q) > 0.1
+                measurement_pos_sphere(t,j,1,:) = (q-p) / norm(q-p);
+            end
         end
     end
 end
@@ -37,50 +69,35 @@ end
 
 %% 第一列第一行
 t = 1;
-subplot(2,5,1);
-[weight_sphere, size_sphere] = fun_plot_particles_sphere_simple(squeeze(sphere_pos_all(t,:,:)), squeeze(measurement_pos_sphere(t,:,:)));
-
-%% 第一列第二行
-subplot(2,5,6);
-fun_plot_particles_simple(squeeze(relative_pos_all(t,:,:)), squeeze(mav_orient_all(t,:,:)), squeeze(measurement_pos_all(t,:,:)), weight_sphere, size_sphere );
+subplot(1,5,1);
+[weight_sphere, size_sphere] = fun_plot_particles_sphere_simple(squeeze(sphere_pos_all(t,:,:,:)), squeeze(measurement_pos_sphere(t,:,:,:)));
+fun_plot_particles_simple(squeeze(mav_pos_all(t,:,:,:)), squeeze(target_pos_all(t,:,:,:)), squeeze(mav_orient_all(t,:,:,:)), squeeze(measurement_pos_all(t,:,:,:)), weight_sphere, size_sphere );
 
 
 %% 第二列第一行
 t = 2;
-subplot(2,5,2);
-[weight_sphere, size_sphere] = fun_plot_particles_sphere_simple(squeeze(sphere_pos_all(t,:,:)), squeeze(measurement_pos_sphere(t,:,:)));
-
-%% 第二列第二行
-subplot(2,5,7);
-fun_plot_particles_simple(squeeze(relative_pos_all(t,:,:)), squeeze(mav_orient_all(t,:,:)), squeeze(measurement_pos_all(t,:,:)), weight_sphere, size_sphere );
+subplot(1,5,2);
+[weight_sphere, size_sphere] = fun_plot_particles_sphere_simple(squeeze(sphere_pos_all(t,:,:,:)), squeeze(measurement_pos_sphere(t,:,:,:)));
+fun_plot_particles_simple(squeeze(mav_pos_all(t,:,:,:)), squeeze(target_pos_all(t,:,:,:)), squeeze(mav_orient_all(t,:,:,:)), squeeze(measurement_pos_all(t,:,:,:)), weight_sphere, size_sphere );
 
 
 %% 第三列第一行
 t = 3;
-subplot(2,5,3);
-[weight_sphere, size_sphere] = fun_plot_particles_sphere_simple(squeeze(sphere_pos_all(t,:,:)), squeeze(measurement_pos_sphere(t,:,:)));
-
-%% 第三列第二行
-subplot(2,5,8);
-fun_plot_particles_simple(squeeze(relative_pos_all(t,:,:)), squeeze(mav_orient_all(t,:,:)), squeeze(measurement_pos_all(t,:,:)), weight_sphere, size_sphere );
+subplot(1,5,3);
+[weight_sphere, size_sphere] = fun_plot_particles_sphere_simple(squeeze(sphere_pos_all(t,:,:,:)), squeeze(measurement_pos_sphere(t,:,:,:)));
+fun_plot_particles_simple(squeeze(mav_pos_all(t,:,:,:)), squeeze(target_pos_all(t,:,:,:)), squeeze(mav_orient_all(t,:,:,:)), squeeze(measurement_pos_all(t,:,:,:)), weight_sphere, size_sphere );
 
 
 %% 第四列第一行
 t = 4;
-subplot(2,5,4);
-[weight_sphere, size_sphere] = fun_plot_particles_sphere_simple(squeeze(sphere_pos_all(t,:,:)), squeeze(measurement_pos_sphere(t,:,:)));
-
-%% 第四列第二行
-subplot(2,5,9);
-fun_plot_particles_simple(squeeze(relative_pos_all(t,:,:)), squeeze(mav_orient_all(t,:,:)), squeeze(measurement_pos_all(t,:,:)), weight_sphere, size_sphere );
+subplot(1,5,4);
+[weight_sphere, size_sphere] = fun_plot_particles_sphere_simple(squeeze(sphere_pos_all(t,:,:,:)), squeeze(measurement_pos_sphere(t,:,:,:)));
+fun_plot_particles_simple(squeeze(mav_pos_all(t,:,:,:)), squeeze(target_pos_all(t,:,:,:)), squeeze(mav_orient_all(t,:,:,:)), squeeze(measurement_pos_all(t,:,:,:)), weight_sphere, size_sphere );
 
 
 %% 第五列第一行
 t = 5;
-subplot(2,5,5);
-[weight_sphere, size_sphere] = fun_plot_particles_sphere_simple(squeeze(sphere_pos_all(t,:,:)), squeeze(measurement_pos_sphere(t,:,:)));
-
-%% 第五列第二行
-subplot(2,5,10);
-fun_plot_particles_simple(squeeze(relative_pos_all(t,:,:)), squeeze(mav_orient_all(t,:,:)), squeeze(measurement_pos_all(t,:,:)), weight_sphere, size_sphere );
+subplot(1,5,5);
+[weight_sphere, size_sphere] = fun_plot_particles_sphere_simple(squeeze(sphere_pos_all(t,:,:,:)), squeeze(measurement_pos_sphere(t,:,:,:)));
+fun_plot_particles_simple(squeeze(mav_pos_all(t,:,:,:)), squeeze(target_pos_all(t,:,:,:)), squeeze(mav_orient_all(t,:,:,:)), squeeze(measurement_pos_all(t,:,:,:)), weight_sphere, size_sphere );
 
--- fun_plot_particles_simple.m
+++ fun_plot_particles_simple.m
@@ -1,54 +1,76 @@
-function fun_plot_particles_simple(relative_pos, mav_orient, measurement_pos, weight_sphere, size_sphere)
+function fun_plot_particles_simple(mav_pos, target_pos, mav_orient, measurement_pos, weight_sphere, size_sphere)
 %画二维粒子分布函数。
 
+    mav_num = size(mav_pos, 1);
+    target_num = size(target_pos, 1);
+    particle_num = size(mav_pos, 2);
+    
     %% 概率分布
-    sigma_p = 0.025;
+    sigma_p = 0.005;
     xmin = -5; xmax = 5;
     ymin = -2; ymax = 12;
     step = 0.1;
-    if norm(measurement_pos) > 0.1
-        a = xmin:step:xmax;
-        b = ymin:step:ymax;
-        [x,y] = meshgrid(a,b);
-        z = zeros(size(x));
-
-        for i = 1:size(x,1)
-            for j = 1:size(x,2)
-                % z(i,j) = sin(x(i,j)) + sin(y(i,j));
-                norm_p = [x(i,j), y(i,j), 0] / norm([x(i,j), y(i,j), 0]);
-                norm_measurement = measurement_pos / norm(measurement_pos);
-                z(i,j) = fun_Gaussian(1.0 - norm_p * norm_measurement, 0, sigma_p) - 5;
+    
+    a = xmin:step:xmax;
+    b = ymin:step:ymax;
+    [x,y] = meshgrid(a,b);
+    z = zeros(size(x));
+    
+    for ii = 1:mav_num
+    for jj = ii:target_num
+        if norm(measurement_pos(jj,:)) > 0.1
+            p = squeeze(mav_pos(ii,1,:));
+            relative_measure = measurement_pos(jj,:)-p';
+            norm_measurement = relative_measure / norm(relative_measure);
+            
+            for i = 1:size(x,1)
+                for j = 1:size(x,2)
+                    % z(i,j) = sin(x(i,j)) + sin(y(i,j));
+                    pt = [x(i,j); y(i,j); 0];
+                    norm_p = (pt-p) / norm(pt-p);
+                    z(i,j) = z(i,j) + fun_Gaussian(1.0 - norm_measurement * norm_p, 0, sigma_p) - 5;
+                end
             end
         end
     end
+    end
 
     %% 算方向
-    particle_num = size(relative_pos, 1);
     L = 1.5;
-    mav_orient_xy = zeros(2, particle_num);
-    mav_orient_xy(1,:) = L * cos(mav_orient);
-    mav_orient_xy(2,:) = L * sin(mav_orient);
+    mav_orient_xy = zeros(mav_num, particle_num, 2);
+    for i = 1:mav_num
+        for l = 1:particle_num
+            mav_orient_xy(i,l,1) = mav_pos(i,l,1) + L * cos(mav_orient(i,l));
+            mav_orient_xy(i,l,2) = mav_pos(i,l,2) + L * sin(mav_orient(i,l));
+        end
+    end
 
     %% 画粒子
     hold on;
-    if norm(measurement_pos) > 0.1
-        scatter(measurement_pos(1), measurement_pos(2), 60, 'MarkerEdgeColor','k', 'MarkerFaceColor',[0.8500, 0.10, 0.0980]);
-        scatter(relative_pos(:,1), relative_pos(:,2), 150*size_sphere(:), 'MarkerEdgeColor',"#f47923", 'MarkerFaceColor',"#ff8d24");
-        surf(x,y,z, 'FaceAlpha',0.5);
-        mymap = [[1:-0.01:0.]; [1:-0.01:0.]; [1:-0.01:0.]]';
-        colormap(gca, mymap);
-%         mymap = [1,1,1; 0.6,0.6,0.6];
-%         colorbar('SouthOutside')
-        shading interp;
-        view(2)
-    else
-        scatter(relative_pos(:,1), relative_pos(:,2), 15, 'MarkerEdgeColor',"#f47923", 'MarkerFaceColor',"#ff8d24");
+    for j = 1:target_num
+        if norm(measurement_pos) > 0.1
+            scatter(measurement_pos(j,1), measurement_pos(j,2), 60, 'MarkerEdgeColor','k', 'MarkerFaceColor',[0.8500, 0.10, 0.0980]);
+            scatter(target_pos(j,:,1), target_pos(j,:,2), 150*size_sphere(j,:), 'MarkerEdgeColor',"#f47923", 'MarkerFaceColor',"#ff8d24");
+            surf(x,y,z, 'FaceAlpha',0.5);
+            mymap = [[1:-0.01:0.]; [1:-0.01:0.]; [1:-0.01:0.]]';
+            colormap(gca, mymap);
+    %         mymap = [1,1,1; 0.6,0.6,0.6];
+    %         colorbar('SouthOutside')
+            shading interp;
+            view(2)
+        else
+            scatter(target_pos(j,:,1), target_pos(j,:,2), 15, 'MarkerEdgeColor',"#f47923", 'MarkerFaceColor',"#ff8d24");
+        end
     end
     
     %% 画圆和箭头
-    viscircles([0,0], 1,'Color','#b1ceef');
-    quiver(zeros(1,particle_num),zeros(1,particle_num), mav_orient_xy(1,:), mav_orient_xy(2,:), ...
-    '-','LineWidth',3,'Color','#77AC30','AutoScale','on','AutoScaleFactor',1);
+    for i = 1:mav_num
+        for l = 1:particle_num
+            viscircles([mav_pos(i,l,1), mav_pos(i,l,2)], 1,'Color','#b1ceef');
+            quiver(mav_pos(i,l,1), mav_pos(i,l,2), mav_orient_xy(i,l,1), mav_orient_xy(i,l,2), ...
+            '-','LineWidth',3,'Color','#77AC30','AutoScale','on','AutoScaleFactor',1);
+        end
+    end
     
     %% 绘图配置
     axis equal;
