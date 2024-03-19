4cc1c5717d591e2f52e5890199514b4458e44dce
kenneth <1269769296@qq.com>
2024-03-19 19:46:29 +0800

    3v3画轨迹

---
+++ data_processing/240318_2239/plot_traj.m
@@ -0,0 +1,58 @@
+clc; clear;
+
+%% 读数据
+mav_num = 2;
+P = struct();
+T = struct();
+for i = 1:mav_num
+    name = strcat("data",num2str(i));
+    load (name);
+    P.(name) = p;
+    T.(name) = t;
+end
+t1 = T.("data1");
+
+%% 长度
+Len = zeros(mav_num,1);
+P_begin = ones(mav_num,1);
+P_end = ones(mav_num,1);
+for i = 1:mav_num
+    name = strcat("data",num2str(i));
+    Len(i) = size(P.(name), 1);
+end
+P_begin(1) = 100;
+P_end(1) = 4200;      % 根据需要修改终止时刻序号，默认Len(1)
+
+%% 确定绘图数据区间
+for i = 2:mav_num
+    name = strcat("data",num2str(i));
+    for j = 1:Len(i)
+        if T.(name)(j) > t1(P_begin(1))
+            P_begin(i) = j;
+            break
+        end
+    end
+end
+for i = 2:mav_num
+    name = strcat("data",num2str(i));
+    for j = Len(i):-1:1
+        if T.(name)(j) < t1(P_end(1))
+            P_end(i) = j;
+            break
+        end
+    end
+end
+
+for i = 1:mav_num
+    name = strcat("data",num2str(i));
+    P.(name) = P.(name)(P_begin(i):P_end(i), :);
+    T.(name) = T.(name)(P_begin(i):P_end(i));
+end
+
+%% 画图
+plot3(P.("data1")(:,1), P.("data1")(:,2), P.("data1")(:,3));
+hold on;
+for i = 2:mav_num
+    name = strcat("data",num2str(i));
+    plot3(P.(name)(:,1), P.(name)(:,2), P.(name)(:,3));
+end
---
+++ data_processing/240319_1748/plot_traj.m
@@ -0,0 +1,60 @@
+clc; clear;close all;
+
+%% 读数据
+mav_num = 6;
+P = struct();
+T = struct();
+for i = 1:mav_num
+    name = strcat("data",num2str(i));
+    load (name);
+    P.(name) = p;
+    T.(name) = t;
+end
+t1 = T.("data1");
+
+%% 长度
+Len = zeros(mav_num,1);
+P_begin = ones(mav_num,1);
+P_end = ones(mav_num,1);
+for i = 1:mav_num
+    name = strcat("data",num2str(i));
+    Len(i) = size(P.(name), 1);
+end
+P_begin(1) = 21300;      % 根据需要修改终止时刻序号，默认Len(1)。起飞到开始降落20000-23000，逆过程21300-25000更好看
+P_end(1) = 25000;
+
+%% 确定绘图数据区间
+for i = 2:mav_num
+    name = strcat("data",num2str(i));
+    for j = 1:Len(i)
+        if T.(name)(j) > t1(P_begin(1))
+            P_begin(i) = j;
+            break
+        end
+    end
+end
+for i = 2:mav_num
+    name = strcat("data",num2str(i));
+    for j = Len(i):-1:1
+        if T.(name)(j) < t1(P_end(1))
+            P_end(i) = j;
+            break
+        end
+    end
+end
+
+for i = 1:mav_num
+    name = strcat("data",num2str(i));
+    P.(name) = P.(name)(P_begin(i):P_end(i), :);
+    T.(name) = T.(name)(P_begin(i):P_end(i));
+end
+
+%% 画图
+plot3(P.("data1")(:,1), P.("data1")(:,2), P.("data1")(:,3));
+hold on;
+for i = 2:mav_num
+    name = strcat("data",num2str(i));
+    plot3(P.(name)(:,1), P.(name)(:,2), P.(name)(:,3));
+end
+
+legend
